7237922137c40085e0da885423c2556bbae356d7
Tomaz Cuk <venooxpersonal@gmail.com>
2020-02-02 18:24:34 +0100

    Preparing env

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,19 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -30,6 +28,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
+  next(createError(404));
 });
 
 module.exports = app;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,37 +9,8 @@ import './App.scss';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
-        <Switch>
-          <Route path="/about">
-            a
-          </Route>
-          <Route path="/users">
-            b
-          </Route>
-          <Route path="/">
-            c
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+    </>
   );
 }
 
